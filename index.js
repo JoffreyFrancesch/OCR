ef34d0bd62d24131fd191d2e0b1331fd3c874b24
JoffreyFrancesch <franceschini@et.esiea.fr>
2017-12-01 21:01:57 +0100

    Try to solve error ApiError: Not Found

---
+++ index.js
@@ -0,0 +1,22 @@
+// Imports the Google Cloud client library
+const Storage = require('@google-cloud/storage');
+
+// Creates a client
+const storage = new Storage();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const bucketName = 'ocr_projet';
+const filename = '/Users/joffrey/Desktop/PST/image.jpg';
+
+// Uploads a local file to the bucket
+storage
+  .bucket(bucketName)
+  .upload(filename)
+  .then(() => {
+    console.log(`${filename} uploaded to ${bucketName}.`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
