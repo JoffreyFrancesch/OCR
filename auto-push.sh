934de2d2b54a5f426fc71e008dacd9008a6f853b
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-01-11 16:22:59 +0100

    Mise a jour

--- auto-push.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "AUTO COMMIT"
-git push -f origin master
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+/**
+ * @Author: Joffrey Francechini <joffrey>
+ * @Date:   01-Dec-2017
+ * @Email:  franceschini@et.esiea.fr
+ * @Filename: index.js
+ * @Last modified by:   joffrey
+ * @Last modified time: 11-Jan-2018
+ */
 // Imports the Google Cloud client library
 const Storage = require('@google-cloud/storage');
+const vision = require('@google-cloud/vision');
+const jsonfile = require('jsonfile');
 
 // Creates a client
 const storage = new Storage();
+const client = new vision.ImageAnnotatorClient();
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
 const bucketName = 'ocr_project';
-const filename = '/Users/joffrey/Desktop/PST/image.jpg';
+const filename = '/Users/joffrey/Desktop/PST/fiche/ESILV.jpeg';
+const json = 'out.json';
 
 // Uploads a local file to the bucket
 storage
@@ -16,6 +25,21 @@ storage
   .upload(filename)
   .then(() => {
     console.log(`${filename} uploaded to ${bucketName}.`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+
+  client
+  .textDetection(filename)
+  .then(results => {
+    const detect = results[0].textAnnotations;
+
+    console.log('Text:');
+    detect.forEach(function(text){
+      jsonfile.writeFileSync(json,text);
+      console.log(text);
+    })
   })
   .catch(err => {
     console.error('ERROR:', err);
