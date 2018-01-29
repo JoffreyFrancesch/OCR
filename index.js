d822a5772e897a760c47676391715e29bc129a6e
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-01-29 12:43:42 +0100

    add PDF converter

--- index.js
+++ index.js
@@ -2,14 +2,27 @@
 const Storage = require('@google-cloud/storage');
 const vision = require('@google-cloud/vision');
 const jsonfile = require('jsonfile');
+const pdf2pic = require('pdf2pic')
 // Creates a client
 const storage = new Storage();
 const client = new vision.ImageAnnotatorClient();
+// Create a converter
+let converter = new pdf2pic({
+  density: 100,
+  savename: "out",
+  savedir:"./Images",
+  format: "jpeg",
+  size: 600
+})
 //file indexing
 const bucketName = 'ocr_project';
 const filename = '/Users/joffrey/Desktop/PST/fiche/ESILV.jpeg';
 const json_description = '/Users/joffrey/Desktop/PST/JSON/out_description.json';
 const json_full = '/Users/joffrey/Desktop/PST/JSON/out_full.json';
+
+converter.convert(filename).then(resolve => {
+  console.log("DONE");
+})
 
 // Uploads a local file to the bucket
 storage
