45a1b373e83952a00eb35d4d0007ff26d91f084f
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-01-29 19:08:17 +0100

    ReadMe

---
+++ README.md
@@ -0,0 +1,54 @@
+PROJET FDP OCR
+==============
+
+Le FDP OCR est un programme sur lequel les intervenants de Platypus
+pourront déposer les feuilles de présence (un scan ou une photo de bonne
+qualité) des cours où ils interviennent. Plutôt que de saisir manuellement les
+informations contenues sur la feuille, qui est en soit une tâche extrêmement
+chronophage à l’échelle de la société Platypus en plus d’être propice aux
+erreurs et aux oublis, le FDP OCR scannera directement le contenu de la
+feuille et en extirpera toutes les données grâce aux dernières technologies de
+reconnaissance de Google dans le cadre de l’analyse et de l’extraction de
+données imprimées et manuscrites. Ces informations seront ensuite traitées puis
+intégrées à la plateforme de dépôt des PV de l’entreprise et permettront le
+suivi administratif (absences, retards, ajout ou retrait d’un étudiant dans un
+groupe de travail pour permettre la mise à jour des listes...), mais aussi
+pédagogique (suivi de l’assiduité, recensement des points de participation...)
+des nombreux élèves qui suivent un enseignement made in Platypus !
+
+Sorti du programme
+------------------
+
+Le projet sortira une fichier JSON qui suivra cette syntaxe :
+
+`JSON
+[
+	{
+metadata : {
+id : 0B7mNn544KuPGaE9CbDNINFJWUlU,
+title :’2017 09 19 - ESIEA 2A SDD - TD 6_30 2B1 - FRANCK’,
+},
+header : {
+activity : ‘TI102-TD/…’,
+session :  ‘Mercredi 20 … E36’,
+assignments : {
+teacher : ‘LEPOIVRE, Franck’,
+students_group : // laisser vide en première approche }
+signature : true
+},
+students : [
+	{
+		name : ABITTAN David,
+		status : P,
+		bonus : 1
+},
+...
+],
+free_part : tout ce qui ne rentre pas dans les champs précédents.		
+}, // fin du premier document
+// document suivant, même structure
+{
+}
+]
+
+`
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Imports the Google Cloud client library and other library
-//const Storage = require('@google-cloud/storage');
 const vision = require('@google-cloud/vision');
 const jsonfile = require('jsonfile');
 const pdf2pic = require('pdf2pic');
+const googleUrl = require('google-url-helper');
 
 
 // Creates a client
-//const storage = new Storage();
 const client = new vision.ImageAnnotatorClient();
 
 
@@ -21,10 +20,14 @@ let converter = new pdf2pic({
 
 
 //file indexing
+//var url = 'https://drive.google.com/file/d/0B7mNn544KuPGSkp3dW8zc01mdW8/view';
+//const id = googleUrl.parseId(url);
+
+const id = 'output';
+
 //const bucketName = 'ocr_project';
 const fileToConvert = '/Users/joffrey/Desktop/PST/fiche/EFREI.pdf';
-const jsonDescription = '/Users/joffrey/Desktop/PST/JSON/out_description.json';
-const jsonFull = '/Users/joffrey/Desktop/PST/JSON/out_full.json';
+const jsonOutput = `/Users/joffrey/Desktop/PST/JSON/${id}.json`;
 var fileName;
 
 //Do the convertion only if is a PDF
@@ -37,35 +40,23 @@ if (fileToConvert.endsWith(".pdf")){
    fileName = fileToConvert;
 }
 
-
-// Uploads a local file to the bucket
-// storage
-//   .bucket(bucketName)
-//   .upload(fileName)
-//   .then(() => {
-//     console.log(`${fileName} uploaded to ${bucketName}.`);
-//   })
-//   .catch(err => {
-//     console.error('ERROR:', err);
-//   });
-
 //First Detection with GOOGLE VISION
   client
   .textDetection(fileName)
   .then(results => {
     const detect = results[0].textAnnotations
     detect.forEach(function(text){
-      //if (text.description.startsWith("TD")){
-      //  jsonfile.writeFileSync(jsonDescription,{ lesson : 'text.description'},{flag : 'a'})
-      //}
-      if (text.description.startsWith("Lundi")){
-        jsonfile.writeFileSync(jsonDescription,{ date : text.description},{flag : 'a'})
-      } else {
-        jsonfile.writeFileSync(jsonDescription,text.description,{flag : 'a'});
+      //formatation du Json
+      if (text.description.startsWith("TD")){
+       jsonfile.writeFileSync(jsonOutput,{ lesson : 'text.description'},{flag : 'a'})
       }
-
-      //jsonfile.writeFileSync(jsonFull,text,{flag : 'a'});
+      if (text.description.startsWith("Lundi")){
+        jsonfile.writeFileSync(jsonOutput,{ date : text.description},{flag : 'a'})
+      } else {
+        jsonfile.writeFileSync(jsonOutput,text.description,{flag : 'a'});
+      }
     })
+    console.log(`Conversion done JSON file save at ${jsonOutput}`)
   })
   .catch(err => {
     console.error('ERROR:', err);
