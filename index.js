2ac0754ac54a8703e82dac18c4efee87af86627f
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-02-08 20:42:29 +0100

    Remove Array Item when there are writing in JSON file

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new vision.ImageAnnotatorClient();
 let converter = new pdf2pic({
   density: 100,
   savename: "FDP",
-  savedir:"./Images",
+  savedir: "./Images",
   format: "jpeg",
   size: 600
 });
@@ -32,45 +32,75 @@ const jsonOutput = `/Users/joffrey/Desktop/PST/JSON/${id}.json`;
 var fileName;
 
 //Do the convertion only if is a PDF
-if (fileToConvert.endsWith(".pdf")){
+if (fileToConvert.endsWith(".pdf")) {
   converter.convert(fileToConvert).then(resolve => {
     console.log(`Conversion done and save at ${fileName}`);
   })
-   fileName = './Images/FDP_1.jpeg';
+  fileName = './Images/FDP_1.jpeg';
 } else {
-   fileName = fileToConvert;
+  fileName = fileToConvert;
 }
 
-
-function writeInFormatJson(text){
+function writeInFormatJsonContent(text, compteur,array,i) {
   //TODO all case need to be implements
-  if(text.match("lundi") || text.match("mardi") || text.match("mercredi") || text.match("jeudi") || text.match("vendredi") || text.match("samedi")){
-    fs.appendFileSync(jsonOutput,`"date" : "${text}",`);
-  } else if (text.match("lepoivre") || text.match("benmessaoud") || text.match("marshall")) {
-    fs.appendFileSync(jsonOutput,`"teacher" : "${text}",`);
-  } else if (text.match("td")) {
-    fs.appendFileSync(jsonOutput,`"lesson" : "${text}",`);
-  } else if (text.match("efrei") || text.match("esiea") || text.match("esilv")) {
-    fs.appendFileSync(jsonOutput,`"school" : "${text}",`);
+  if (compteur < 3) {
+    if (text.match("lundi") || text.match("mardi") ||  text.match("mercredi") || text.match("jeudi") || text.match("vendredi") || text.match("samedi")) {
+      fs.appendFileSync(jsonOutput, `"date" : "${text}",`);
+      array.splice(i,1);
+      return true;
+    } else if (text.match("lepoivre") || text.match("benmessaoud") || text.match("marshall")) {
+      fs.appendFileSync(jsonOutput, `"teacher" : "${text}",`);
+      array.splice(i,1);
+      return true;
+    } else if (text.match("td")) {
+      fs.appendFileSync(jsonOutput, `"lesson" : "${text}",`);
+      array.splice(i,1);
+      return true;
+    } else if (text.match("efrei") ||  text.match("esiea") || text.match("esilv")) {
+      fs.appendFileSync(jsonOutput, `"school" : "${text}",`);
+      array.splice(i,1);
+      return true;
+    }
   } else {
-    console.log(text);
+    if (text.match("lundi") || text.match("mardi") ||  text.match("mercredi") || text.match("jeudi") || text.match("vendredi") || text.match("samedi")) {
+      fs.appendFileSync(jsonOutput, `"date" : "${text}"`);
+      array.splice(i,1);
+    } else if (text.match("lepoivre") || text.match("benmessaoud") || text.match("marshall")) {
+      fs.appendFileSync(jsonOutput, `"teacher" : "${text}"`);
+      array.splice(i,1);
+    } else if (text.match("td")) {
+      fs.appendFileSync(jsonOutput, `"lesson" : "${text}"`);
+      array.splice(i,1);
+    } else if (text.match("efrei") ||  text.match("esiea") || text.match("esilv")) {
+      fs.appendFileSync(jsonOutput, `"school" : "${text}"`);
+      array.splice(i,1);
+    }
   }
+
+}
+
+function remove(array, element) {
+  const index = array.indexOf(element);
+  array.splice(index, 1)
 }
 //First Detection with GOOGLE VISION
-  client
+client
   .documentTextDetection(fileName)
   .then(results => {
     var detect = results[0].fullTextAnnotation
     var detectTable = detect.text.split("\n");
     var i = 0;
-    fs.appendFileSync(jsonOutput,"{ \"content\" : {");
-    while (i < detectTable.length){
-      //console.log(detectTable[i]);
-
-      writeInFormatJson(detectTable[i].toLowerCase());
+    var compteur = 0;
+    fs.appendFileSync(jsonOutput, "{ \"content\" : {");
+    while (i < detectTable.length) {
+      if (writeInFormatJsonContent(detectTable[i].toLowerCase(), compteur, detectTable,i)) {
+        compteur += 1;
+      }
+      console.log(detectTable[i]);
       i++;
     }
-    fs.appendFileSync(jsonOutput,'}')
+    console.log(detectTable);
+    fs.appendFileSync(jsonOutput, '}}')
     console.log(`Conversion done JSON file save at ${jsonOutput}`)
   })
   .catch(err => {
