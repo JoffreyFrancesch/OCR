511601eee7c2f0dc85705e5d370d4e4819c9006c
JoffreyFrancesch <franceschini@et.esiea.fr>
2018-03-26 16:13:50 +0200

    efrei.js copy code

--- JSfile/efrei.js
+++ JSfile/efrei.js
@@ -1,13 +1,43 @@
 exports.writeHeader = function (text, compteur) {
-  // body...
+  if (compteur < 3) {
+    if (text.match("lundi") || text.match("mardi") || text.match("mercredi") || text.match("jeudi") || text.match("vendredi") || text.match("samedi")) {
+      fs.appendFileSync(jsonOutput, `"date" : "${text}",`);
+      return true;
+    } else if (text.match("lepoivre") || text.match("benmessaoud") || text.match("marshall")) {
+      fs.appendFileSync(jsonOutput, `"teacher" : "${text}",`);
+      teacherName = text;
+      return true;
+    } else if (text.match("td")) {
+      fs.appendFileSync(jsonOutput, `"lesson" : "${text}",`);
+      return true;
+    } else if (text.match("efrei") || text.match("esiea") || text.match("esilv")) {
+      fs.appendFileSync(jsonOutput, `"school" : "${text}",`);
+      return true;
+    }
+  } else {
+    writeHeaderEnd(text);
+  }
 };
 
 exports.writeHeaderEnd = function (text) {
-  // body...
+  if (text.match("lundi") || text.match("mardi") || text.match("mercredi") || text.match("jeudi") || text.match("vendredi") || text.match("samedi")) {
+    fs.appendFileSync(jsonOutput, `"date" : "${text}"`);
+  } else if (text.match("lepoivre") || text.match("benmessaoud") || text.match("marshall")) {
+    fs.appendFileSync(jsonOutput, `"teacher" : "${text}"`);
+    teacherName = text;
+  } else if (text.match("td")) {
+    fs.appendFileSync(jsonOutput, `"lesson" : "${text}"`);
+  } else if (text.match("efrei") || text.match("esiea") || text.match("esilv")) {
+    fs.appendFileSync(jsonOutput, `"school" : "${text}"`);
+  }
 };
 
 exports.writeStudent = function (text) {
-  // body...
+  if (text == teacherName) {
+    return;
+  } else {
+    fs.appendFileSync(jsonOutput, `{"name" : "${text}"},`);
+  }
 };
 
 exports.writeMetaData = function (id, title) {
